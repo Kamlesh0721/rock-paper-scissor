baeb9fd03a2bc21f06ea8fdf798ae5cde950db87
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-09-05 13:16:48 +0530

    add choice-css

---
+++ script.js
@@ -0,0 +1,58 @@
+// Rock : 0 | Paper : 1 | Scissor : 2
+const scoreBoard = {
+    win: 0,
+    loose: 0,
+    tie: 0,
+    win_streak: 0,
+  };
+
+  let winning_streak = false;
+  function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+      return "rock";
+    } else if (choice === 1) {
+      return "paper";
+    } else {
+      return "scissor";
+    }
+  }
+
+  function calcResult(computerChoice, userChoice) {
+    if (computerChoice === userChoice) {
+      return -1;
+    } else if (
+      (computerChoice === "rock" && userChoice === "paper") ||
+      (computerChoice === "paper" && userChoice === "scissor") ||
+      (computerChoice === "scissor" && userChoice === "rock")
+    ) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  function playGame(computerChoice, userChoice) {
+    let result = calcResult(computerChoice, userChoice);
+    if (result === -1) {
+      scoreBoard.tie = scoreBoard.tie + 1;
+      winning_streak = false;
+    } else if (result === 0) {
+      scoreBoard.loose = scoreBoard.loose + 1;
+      winning_streak = false;
+      scoreBoard.win_streak = 0;
+    } else {
+      scoreBoard.win = scoreBoard.win + 1;
+      scoreBoard.win_streak = scoreBoard.win_streak + 1;
+    }
+
+    updateScoreBoard();
+  }
+
+  function updateScoreBoard() {
+    document.getElementById("win-score").textContent = scoreBoard.win;
+    document.getElementById("loose-score").textContent = scoreBoard.loose;
+    document.getElementById("tie-score").textContent = scoreBoard.tie;
+    document.getElementById("win-streak").textContent =
+      scoreBoard.win_streak;
+  }
